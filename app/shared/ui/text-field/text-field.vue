31e9288f74b484852163afd54db870499d030662
Qwimi <133797329+Qwimi@users.noreply.github.com>
2025-11-27 19:26:26 +0300

    feat: add validation (#28)

--- app/shared/ui/text-field/text-field.vue
+++ app/shared/ui/text-field/text-field.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="text-input">
+  <div
+    v-cursor="{stylePreset: 'colorBurn'}"
+    class="text-input"
+    :class="{ 'text-input--error': error }"
+  >
     <input
       v-model="modelValue"
       v-maska
@@ -7,7 +11,16 @@
       class="text-input__field"
       :placeholder="placeholder"
       :data-maska="mask"
+      @blur="emit('blur')"
     />
+    <transition name="fade">
+      <p
+        v-show="error"
+        class="text-input__error"
+      >
+        {{ error }}
+      </p>
+    </transition>
   </div>
 </template>
 
@@ -15,13 +28,16 @@
 import { vMaska } from 'maska/vue'
 
 const modelValue = defineModel<string>({ default: '' })
+const emit = defineEmits(['blur'])
 
 withDefaults(defineProps<{
   placeholder?: string;
   mask?: string;
+  error?: string;
 }>(), {
     placeholder: '',
     mask: '',
+    error: undefined
 });
 
 </script>
@@ -53,7 +69,34 @@ withDefaults(defineProps<{
       color: $text-additional;
     }
   }
+
+  &--error {
+    color: $error;
+
+    .text-input__field {
+      border-bottom-color: $error;
+    }
+  }
+
+  &__error {
+    @include link2;
+
+    overflow: hidden;
+    margin-top: 8px;
+  }
 }
 
+.fade-enter-active,
+.fade-leave-active {
+  transition: max-height 0.3s ease, margin-top 0.3s ease, opacity 0.3s ease;
+}
 
+.fade-enter-to, .fade-leave-from {
+  max-height: 100px;
+}
+
+.fade-leave-to, .fade-enter-from {
+  max-height: 0;
+  margin-top: 0;
+}
 </style>

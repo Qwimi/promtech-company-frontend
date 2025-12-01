cb99205ff5925079295dae17c3ba9514feca2264
Danya_Mochalov <volzhankaonelove@yandex.ru>
2025-12-01 17:19:40 +0400

    feat: technique catalog page added

--- app/pages/technique-catalog/index.vue
+++ app/pages/technique-catalog/index.vue
@@ -1,39 +1,43 @@
 <template>
-  <div class="container">
-    <h1
-      class="page-info__title"
-      :style="{ 'margin-bottom': '20px' }"
-    >
-      Техника
-    </h1>
-    <CategoryPicker
-      v-model="selectedCategoryId"
-      :categories="categories"
-    />
-    <ul>
-      <li
-        v-for="n in 10"
-        :key="n"
-        :style="{ 'margin-bottom': '20px' }"
+  <div class="container technique-catalog">
+    <div class="page-info">
+      <span class="technique-catalog__title page-info__title">
+        Каталог техники
+      </span>
+      <span class="technique-catalog__subtitle page-info__subtitle">
+        Техника, которая работает под землёй и не подводит
+      </span>
+    </div>
+
+    <div class="technique-catalog__content">
+      <CategoryPicker
+        v-model="selectedCategoryId"
+        :categories="categories"
+      />
+
+      <div
+        v-if="machinesInCurrentCategory?.length"
+        class="technique-catalog__machines"
       >
-        <NuxtLink
-          :to="`/components-catalog/${n}`"
-          class="section-label"
+        <div
+          v-for="machine in machinesInCurrentCategory"
+          :key="machine.id"
         >
-          Техника {{ n }}
-        </NuxtLink>
-      </li>
-    </ul>
+          <TechniqueCard :card="machine" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {CategoryPicker} from '~/widgets/technique-catalog';
 import {useCatalogStore} from '~/entities';
+import {TechniqueCard} from '~/shared';
 
 const catalogStore = useCatalogStore();
 
-const { categories } = storeToRefs(catalogStore);
+const { categories, machinesInCurrentCategory } = storeToRefs(catalogStore);
 const selectedCategoryId = ref<number | string | null>(categories.value?.[0]?.id ?? null);
 
 await useAsyncData('categories', async () => {
@@ -46,4 +50,67 @@ await useAsyncData('categories', async () => {
     return true;
 });
 
+watch(selectedCategoryId, async (newId) => {
+    if (newId) {
+        await catalogStore.fetchCategoryMachines(newId.toString());
+    }
+});
+
 </script>
+
+<style lang="scss">
+  .technique-catalog {
+    padding-top: 60px;
+    padding-bottom: 60px;
+    display: flex;
+    flex-direction: column;
+    gap: 60px;
+
+    @media (min-width: $breakpoint-tablet) {
+      gap: 100px;
+    }
+
+    @media (min-width: $breakpoint-desktop) {
+      gap: 128px;
+      padding-top: 128px;
+      padding-bottom: 128px;
+    }
+    
+    &__subtitle {
+      width: 55%;
+
+      @media (min-width: $breakpoint-tablet) {
+        width: 100%;
+      }
+    }
+
+    &__content {
+      display: flex;
+      flex-direction: column;
+      gap: 20px;
+
+      @media (min-width: $breakpoint-tablet) {
+        gap: 40px;
+      }
+
+      @media (min-width: $breakpoint-desktop) {
+        gap: 80px;
+      }
+    }
+
+    &__machines {
+      display: grid;
+      grid-template-columns:1fr;
+      row-gap: 40px;
+
+      @media (min-width: $breakpoint-tablet) {
+        grid-template-columns:1fr 1fr;
+        column-gap: 20px;
+      }
+
+      @media (min-width: $breakpoint-desktop) {
+        grid-template-columns:1fr 1fr 1fr;
+      }
+    }
+  }
+</style>
